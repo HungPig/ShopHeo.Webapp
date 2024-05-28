8fe5834c85d309bd9f0a54af9c22f4d8ce8856d9
Thanhhung <HungPig>
2024-05-29 06:36:58 +0700

    Add project files.

---
+++ ShopHeo.Webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShopHeo.Webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
