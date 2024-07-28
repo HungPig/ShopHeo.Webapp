2770317bbfdc973940a934ab64fc2d295adaa7c5
Thanhhung <HungPig>
2024-07-28 23:44:34 +0700

    "update again all"

---
+++ ShopHeo.Untitiles/Exceptions/HshopExceptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ShopHeo.Untitiles.Exceptions
+{
+    public class HshopExceptions : Exception
+    {
+        public HshopExceptions()
+        {
+        }
+
+        public HshopExceptions(string message)
+            : base(message)
+        {
+        }
+
+        public HshopExceptions(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
